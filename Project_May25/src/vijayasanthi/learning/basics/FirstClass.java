bf9c79f00558ce2948b3b7e7ee0715886562c7e3
vijayasanthi jothimani <vijayasanthi20@gmail.com>
2023-05-25 09:05:53 +0100

    committing first java program from eclipse

---
+++ Project_May25/src/vijayasanthi/learning/basics/FirstClass.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package vijayasanthi.learning.basics;
+
+/**
+ * @author viji
+ *
+ */
+public class FirstClass {
+	
+
+public static void main(String[] args) {
+	FirstClass fc=new  FirstClass();
+	fc.buy();
+	
+}
+
+private void buy() {
+	
+	System.out.println("Hi");
+	
+}
+
+}
