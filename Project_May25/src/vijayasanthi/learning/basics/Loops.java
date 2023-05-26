058c085ff4cb18901f483cb041d484d242f811b8
vijayasanthi jothimani <vijayasanthi20@gmail.com>
2023-05-26 09:04:38 +0100

    pull from remote

---
+++ Project_May25/src/vijayasanthi/learning/basics/Loops.java
@@ -0,0 +1,23 @@
+package vijayasanthi.learning.basics;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+
+int mi = 200; 
+	int gt = 201; 
+	if(mi>gt)
+	{
+		System.out.println("MI enters finals");
+	}
+	else
+	{
+		System.out.println("GT enters finals");
+	}
+
+	}
+
+}
